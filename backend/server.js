88b5d9717f178a9ca6c765af3395d68034487887
Shadman Rahman <shadmxn@gmail.com>
2025-03-05 13:54:19 -0500

    update port

--- backend/server.js
+++ backend/server.js
@@ -9,4 +9,7 @@ app.use(cors());
 
 app.use("/api", generateRoute);
 
-app.listen(5000, () => console.log("Backend running on http://localhost:5000"));
+const PORT = process.env.PORT || 5001; // Use 5001 instead of 5000
+app.listen(PORT, () =>
+  console.log(`Backend running on http://localhost:${PORT}`)
+);
